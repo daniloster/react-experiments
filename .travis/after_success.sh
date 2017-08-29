77525d708ef4d386f30d309e427c5ec6e75b7ec8
Danilo Castro <daniloster@gmail.com>
2017-08-29 08:47:20 +0100

    [release=patch] added auth npm and git

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -14,15 +14,22 @@ fi
 if [[ $TRAVIS_BRANCH == 'master' ]]; then
   echo '** Generating npm auth'
   echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" >> ~/.npmrc
+  git remote add gh-publish "https://$GIT_AUTH_TOKEN@github.com/daniloster/react-experiments.git"
+
+  git config --global user.email "daniloster@gmail.com"
+  git config --global user.name "Danilo Castro"
+  git fetch gh-publish
+  git checkout --track -b master gh-publish/master
+
   export TYPE_RELEASE="$(git log -1 --pretty=%B | grep release= | awk '{print $1}' | sed s/release=//)"
   if [[ $TYPE_RELEASE == '[major]' ]]; then
     echo '** Releasing MAJOR'
-    lerna publish --cd-version=major --yes --message="[skip ci] [release]: %s"
+    lerna publish --cd-version=major --yes --git-remote gh-publish --message="[skip ci] [release]: %s"
   elif [[ $TYPE_RELEASE == '[minor]' ]]; then
     echo '** Releasing MINOR'
-    lerna publish --cd-version=minor --yes --message="[skip ci] [release]: %s"
+    lerna publish --cd-version=minor --yes --git-remote gh-publish --message="[skip ci] [release]: %s"
   elif [[ $TYPE_RELEASE == '[patch]' ]]; then
     echo '** Releasing PATCH'
-    lerna publish --cd-version=patch --yes --message="[skip ci] [release]: %s"
+    lerna publish --cd-version=patch --yes --git-remote gh-publish --message="[skip ci] [release]: %s"
   fi
 fi
